18e94463ac123234571bf192d28904ff0abb141b
alvaralz <a01635348@itesm.mx>
2021-08-08 12:01:26 -0500

    Ejemplos de vectores

---
+++ Ejemplos de vectores.R
@@ -0,0 +1,7 @@
+#Codes for creating vector using c() function()
+
+z<-c(1,2)         #Numeric
+z<-c(TRUE,FALSE)  #Logical
+z<-c("x","y","z") #Charcater
+z<-1:10           #Interger
+z<-c(1+0i,2_1i)   #Complex
